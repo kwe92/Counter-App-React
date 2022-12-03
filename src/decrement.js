066f089c965a780130b4be8d596fb1834d12367a
Kweayon  Clark <Kweayon@gmail.com>
2022-12-03 01:03:18 -0600

    Added decrement.js

---
+++ src/decrement.js
@@ -0,0 +1,9 @@
+import styles from "./style";
+import Button from "@mui/material/Button";
+const DecrementButton = (props) => (
+  <Button sx={styles.buttonStyle} variant="contained" onClick={props.onClick}>
+    Decrement
+  </Button>
+);
+
+export default DecrementButton;
