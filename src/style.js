aaf88bf1c7b2fd1309f60f0bc0f62941bd00cd2c
Kweayon  Clark <Kweayon@gmail.com>
2022-12-03 01:05:06 -0600

    Added style.js

---
+++ src/style.js
@@ -0,0 +1,38 @@
+// const width = 110;
+
+const styles = {
+  //  elements and fill entire screen with the div
+  mainDivStyle: {
+    // div bg color
+    backgroundColor: "#ede9e8",
+    display: "flex",
+    alignItems: "center",
+    // the direction of your flex box | column or row
+    flexDirection: "column",
+    height: "100%",
+    width: "100%",
+    // needed to make dive take up entire screen
+    position: "fixed",
+    top: 0,
+    left: 0,
+  },
+
+  headDiveStyle: {
+    backgroundColor: "#a32cc4",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+
+  h1: {
+    color: "white",
+  },
+  buttonStyle: {
+    width: "60%",
+    backgroundColor: "purple",
+    margin: 0.5,
+  },
+};
+
+export default styles;
